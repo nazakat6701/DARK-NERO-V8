60e5c007ec9b43a32a3227d34d7c85fea9eba804
nazakat6701 <125798057+nazakat6701@users.noreply.github.com>
2023-03-18 21:36:24 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,21 +17,21 @@ global.doc6 = 'application/vnd.android.package-archive'
 global.ANTI_BADWORD = 'true' // true of false
 global.AUTO_REACT = 'false' // true of false
 global.INBOX_BLOCK_MSG ='BLOCK !!!'
-global.INBOX_BLOCK = 'off' //inbox block
-global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
+global.INBOX_BLOCK = 'on' //inbox block
+global.alivemsg = 'Rifo'
 global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
-global.ytname = "YT: Dark" //ur yt chanel name
-global.socialm = "GitHub: Dark" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['03114197498'] //ur owner number
+global.ownername = "Nazakat" //ur owner name
+global.ytname = "Nature Ways" //ur yt chanel name
+global.socialm = "nazakat6701" //ur github or insta name
+global.location = "Chunian Kasur" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
-global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
+global.session = "a3hIM1dmQ1M=" //session Id
+global.ownernomer = "923114197498" //ur number
+global.lang = "Nazakat" //Select Language "EN" to English "SI" to sinhala 
+global.premium = ['923114197498'] //ur premium number
+global.botname = 'Rifo' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://www.darknero.ga/' //script link
